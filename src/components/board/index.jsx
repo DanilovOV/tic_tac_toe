e0314f88e80455318879aab0fff896b91a879083
Oleg Danilov <oleg-danilov8@ya.ru>
2022-05-31 23:03:40 +0300

    upgrade-ui

--- src/components/board/index.jsx
+++ src/components/board/index.jsx
@@ -10,7 +10,7 @@ export default class Board extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="board-box">
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
