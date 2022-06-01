2221f3c4579bc9c8eb24252c4227f55a8ee95513
Oleg Danilov <oleg-danilov8@ya.ru>
2022-06-01 21:36:01 +0300

    add-icons

--- src/components/square/index.jsx
+++ src/components/square/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './square.scss';
+import cross from '../../assets/img/cross.svg';
+import circle from '../../assets/img/circle.svg';
 
 const Square = (props) => {
   return (
     <button className="square" onClick={() => props.onClick()}>
-      {props.value}
+      {props.value === null && false}
+      {props.value === 'X' && <img src={cross} alt="cross" />}
+      {props.value === 'O' && <img src={circle} alt="circle" />}
     </button>
   );
 };
